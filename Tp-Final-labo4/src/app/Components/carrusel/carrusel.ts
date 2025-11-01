f077779ac4f841dd5c360fd159d7ffdf54f97f77
david valdez <davidv77@gmail.com>
2025-11-01 19:44:01 -0300

    carrusel ,servicioplicula,interfaces

--- Tp-Final-labo4/src/app/Components/carrusel/carrusel.ts
+++ Tp-Final-labo4/src/app/Components/carrusel/carrusel.ts
@@ -1,11 +1,60 @@
-import { Component } from '@angular/core';
-
+import { Component,OnInit } from '@angular/core';
+import { TmdbService } from '../../Services/tmdb.service';
+import { Moviein } from '../../Interfaces/moviein';
 @Component({
   selector: 'app-carrusel',
   imports: [],
   templateUrl: './carrusel.html',
   styleUrl: './carrusel.css',
 })
-export class Carrusel {
 
+export class Carrusel implements OnInit {
+  topRatedMovies: Moviein[] = [];
+  currentSlideIndex: number = 0;
+  maxVisibleMovies: number = 5;
+
+  constructor(private tmdbService: TmdbService) {}
+
+  ngOnInit() {
+    this.loadTopRatedMovies();
+  }
+
+  loadTopRatedMovies() {
+    this.tmdbService.getTopRatedMovies().subscribe(
+      (response: any) => {
+        if (response && response.results) {
+          this.topRatedMovies = response.results.slice(0, 10);
+          console.log('Películas cargadas:', this.topRatedMovies);
+        }
+      },
+      (error) => {
+        console.error('Error al obtener las películas:', error);
+      }
+    );
+  }
+
+  prevSlide() {
+    if (this.currentSlideIndex > 0) {
+      this.currentSlideIndex--;
+    }
+    this.updateCarousel();
+  }
+
+  nextSlide() {
+    const maxIndex = this.topRatedMovies.length - this.maxVisibleMovies;
+    if (this.currentSlideIndex < maxIndex) {
+      this.currentSlideIndex++;
+    }
+    this.updateCarousel();
+  }
+
+  updateCarousel() {
+    const movieList = document.querySelector('.movie-list') as HTMLElement;
+    const movieWidth = 150 + 15; 
+    movieList.style.transform = `translateX(-${this.currentSlideIndex * movieWidth}px)`;
+  }
+
+  isNextButtonDisabled(): boolean {
+    return this.currentSlideIndex >= this.topRatedMovies.length - this.maxVisibleMovies;
+  }
 }
--- Tp-Final-labo4/src/app/Services/tmdb.service.ts
+++ Tp-Final-labo4/src/app/Services/tmdb.service.ts
@@ -1,8 +1,26 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TmdbService {
+   private apiKey: string = 'c130076811f0e957626523dba642db29';
+  private baseUrl: string = 'https://api.themoviedb.org/3';
+
+  constructor(private http: HttpClient) {}
+
+  searchMovies(query: string): Observable<any> {
+    return this.http.get(`${this.baseUrl}/search/movie?api_key=${this.apiKey}&query=${query}`);
+  }
+
+  getTopRatedMovies(): Observable<any> {
+    return this.http.get(`${this.baseUrl}/movie/top_rated?api_key=${this.apiKey}&language=es-US&page=1`);
+  }
+  getMovieDetails(movieId: number): Observable<any> {
+    return this.http.get(`${this.baseUrl}/movie/${movieId}?api_key=${this.apiKey}&language=es-US`);
+  }
+
   
 }
