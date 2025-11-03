6876fa84f16e75aa563aadf1083bd2d757a7ca26
Fernanda <mfqaprogra@gmail.com>
2025-11-03 20:28:22 -0300

    login.TS - Formulario

--- Tp-Final-labo4/src/app/Components/login/login.ts
+++ Tp-Final-labo4/src/app/Components/login/login.ts
@@ -1,11 +1,43 @@
-import { Component } from '@angular/core';
+import { Component,inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder,Validators,ReactiveFormsModule} from '@angular/forms';
+import { ProfileService } from '../../Services/profile.service';
+import { Profile } from '../../Interfaces/profilein';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css',
 })
 export class Login {
+
+  private formBuilder = inject(FormBuilder);
+
+  form = this.formBuilder.group({
+    username: ['', [Validators.required]],
+    password: ['', [Validators.required]]
+  })
+  
+
+  constructor(
+    private profileService: ProfileService,
+    private router: Router
+  ) { }
+
+  onLogin() {
+    if (this.form.invalid) return;
+
+    const user = this.form.getRawValue() as Profile;
+
+    this.profileService.login(user).subscribe({
+      next: (loggedin) => {
+        if (loggedin) {
+          this.router.navigate(['/']);
+        } 
+      }
+    })
+  }
+
 
 }
