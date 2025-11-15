c317f1f464a6ab54eb1f8683dbf50d8151b21b36
maurisebastian <21maurimartinez@gmail.com>
2025-11-14 22:32:15 -0300

    creacio de top-bar y funcionamiento

--- Tp-Final-labo4/src/app/Components/top-bar/top-bar.ts
+++ Tp-Final-labo4/src/app/Components/top-bar/top-bar.ts
@@ -1,11 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormControl, ReactiveFormsModule } from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-top-bar',
-  imports: [],
+  imports: [ReactiveFormsModule, RouterLink],
   templateUrl: './top-bar.html',
   styleUrl: './top-bar.css',
 })
 export class TopBar {
 
+  private readonly router = inject(Router);
+
+  busqueda = new FormControl('');
+
+  buscar() {
+    const value = this.busqueda.value ?? ""; 
+    this.router.navigate(['/search', value]);
+  }
+
 }
+
+
+
+
+
+
+
+
--- Tp-Final-labo4/src/app/app.routes.ts
+++ Tp-Final-labo4/src/app/app.routes.ts
@@ -3,12 +3,16 @@ import { Login } from './Components/login/login';
 import { Carrusel } from './Components/carrusel/carrusel';
 import { Signup } from './Components/signup/signup';
 import { MovieReview } from './Components/movie-review/movie-review';
+import { ProfileDetail } from './Components/profile-detail/profile-detail';
+import { MovieSearch } from './Components/movie-search/movie-search';
 
 export const routes: Routes = [
     {path: '', component: Carrusel},
     {path:'movie-review/:id',component: MovieReview},
     {path: 'login', component: Login },
     {path: 'signup', component: Signup},
+    {path: 'profile-detail',component : ProfileDetail },
+    {path: 'search/:query',component : MovieSearch},
     {path: '**',redirectTo: '', pathMatch: 'full'}
 
 ];
